cde3761c4524c752a92c2fc40c429cf650912863
Vladimir <fruityflunt@gmail.com>
2023-09-30 13:39:18 +0300

    Добавил ещё тестов

--- labs/lab1/examples/test.bat
+++ labs/lab1/examples/test.bat
@@ -12,6 +12,18 @@ fc.exe output.txt correct-output.txt > nul
 if ERRORLEVEL 1 goto err
 cd ..
 
+cd mealy-empty-move
+node ../../../../dist/conversion.bundle.js
+fc.exe output.txt correct-output.txt > nul
+if ERRORLEVEL 1 goto err
+cd ..
+
+cd moore-empty-move
+node ../../../../dist/conversion.bundle.js
+fc.exe output.txt correct-output.txt > nul
+if ERRORLEVEL 1 goto err
+cd ..
+
 echo Program testing succeeded
 pause
 exit 0
